95df0f256613dafdcddc53617ad4effdc6e49951
Александр Морев <a.morev@kryptonite.ru>
2023-01-15 02:31:37 +0300

    Create react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
