ceac3d8b984e8ffa1497a92cce715cb45cc4b782
Александр Морев <a.morev@kryptonite.ru>
2023-01-15 02:56:53 +0300

    Create project file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import App from "./App";
+import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
