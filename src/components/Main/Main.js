156a18ea3879f79fd93b76b80f395730c064ffdf
Александр Морев <a.morev@kryptonite.ru>
2023-01-16 00:53:58 +0300

    Add AboutProject component, closes #4

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import "./Main.css";
 
+import AboutProject from "../AboutProject/AboutProject";
 import Promo from "../Promo/Promo";
 
 function Main() {
   return (
     <main className="content">
       <Promo />
+      <AboutProject />
     </main>
   );
 }
