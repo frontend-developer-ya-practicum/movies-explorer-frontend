6d74f3cb6e36d914ca4bfe19ac6344d5d778fde7
Александр Морев <a.morev@kryptonite.ru>
2023-01-15 04:29:50 +0300

    Add inter fonts, normalize.css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.querySelector(".root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
