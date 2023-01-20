1e4c80c3d741a0fa3dc1094c2af2f8755bc6a5f9
Александр Морев <a.morev@kryptonite.ru>
2023-01-20 05:08:23 +0300

    Add Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.svg";
+
+function Logo() {
+  return (
+    <Link to="/" className="logo">
+      <img src={logo} alt="логотип" />
+    </Link>
+  );
+}
+
+export default Logo;
