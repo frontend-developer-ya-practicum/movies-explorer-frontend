af73fa224fb1bb1d980d554de45af72d14034c08
Александр Морев <a.morev@kryptonite.ru>
2023-01-21 17:02:04 +0300

    Add SearchForm component

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,27 @@
+import "./SearchForm.css";
+
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+
+function SearchForm() {
+  return (
+    <section className="search">
+      <form className="search__form">
+        <div className="search__icon"></div>
+        <input
+          className="search__input"
+          id="search-input"
+          type="text"
+          placeholder="Фильм"
+          required
+        ></input>
+        <button className="search__submit-btn" type="submit"></button>
+      </form>
+
+      <div className="search__checkbox">
+        <FilterCheckbox text="Короткометражки" />
+      </div>
+    </section>
+  );
+}
+
+export default SearchForm;
