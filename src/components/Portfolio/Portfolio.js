89e86771cd18556d865a1ff1bc1edafd4c8a14e0
Александр Морев <a.morev@kryptonite.ru>
2023-01-22 22:29:32 +0300

    Fix Portfolio link list

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,37 +4,43 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h3 className="portfolio__title">Портфолио</h3>
-      <nav className="portfolio__links">
-        <a
-          href="https://frontend-developer-ya-practicum.github.io/how-to-learn/"
-          className="portfolio__link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p className="portfolio__text">Статичный сайт</p>
-          <p className="portfolio__arrow">↗</p>
-        </a>
+      <ul className="portfolio__items">
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://frontend-developer-ya-practicum.github.io/how-to-learn/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__text">Статичный сайт</p>
+            <p className="portfolio__arrow">↗</p>
+          </a>
+        </li>
 
-        <a
-          href="https://frontend-developer-ya-practicum.github.io/russian-travel/"
-          className="portfolio__link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p className="portfolio__text">Адаптивный сайт</p>
-          <p className="portfolio__arrow">↗</p>
-        </a>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://frontend-developer-ya-practicum.github.io/russian-travel/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__text">Адаптивный сайт</p>
+            <p className="portfolio__arrow">↗</p>
+          </a>
+        </li>
 
-        <a
-          href="https://frontend-developer-ya-practicum.github.io/mesto/"
-          className="portfolio__link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p className="portfolio__text">Одностраничное приложение</p>
-          <p className="portfolio__arrow">↗</p>
-        </a>
-      </nav>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://frontend-developer-ya-practicum.github.io/mesto/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__text">Одностраничное приложение</p>
+            <p className="portfolio__arrow">↗</p>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
