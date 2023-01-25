10a3029e88f90638747d973609dd38e19f964798
Александр Морев <a.morev@kryptonite.ru>
2023-01-25 04:24:30 +0300

    Add signin impl, closes #23

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,18 +2,49 @@ import "./Login.css";
 
 import AuthForm from "../AuthForm/AuthForm";
 import AuthFormInput from "../AuthFormInput/AuthFormInput";
+import { useState } from "react";
 
-function Login() {
+function Login({ onLogin, apiError }) {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onLogin(credentials);
+  }
+
   return (
     <AuthForm
+      apiError={apiError}
+      onSubmit={handleSubmit}
       title="Рады видеть!"
       submitButtonText="Войти"
       formText="Ещё не зарегистрированы?"
       formLink="/signup"
       formLinkText="Регистрация"
     >
-      <AuthFormInput title="E-mail" type="email" id="email" name="email" />
       <AuthFormInput
+        value={credentials.email}
+        onChange={handleChange}
+        title="E-mail"
+        type="email"
+        id="email"
+        name="email"
+      />
+      <AuthFormInput
+        value={credentials.password}
+        onChange={handleChange}
         title="Пароль"
         type="password"
         id="password"
