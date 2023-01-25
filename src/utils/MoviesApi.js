24fda05f0b7e5320d4ce5b537fb85e3bd43a96b1
Александр Морев <a.morev@kryptonite.ru>
2023-01-26 02:53:21 +0300

    Add MoviesApi, ref #18

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,29 @@
+class MoviesApi {
+  constructor(url, headers) {
+    this._url = url;
+    this._headers = headers;
+    this._image_url = this._url.substring(0, this._url.lastIndexOf("/"));
+  }
+
+  async getMovies() {
+    const resp = await fetch(this._url, {
+      headers: this._headers,
+      method: "GET",
+    });
+
+    const data = await resp.json();
+    if (resp.ok) {
+      data.forEach((movie) => {
+        movie.image.url = this._image_url + movie.image.url;
+      });
+      return data;
+    }
+    return Promise.reject(data.message);
+  }
+}
+
+const moviesApi = new MoviesApi(process.env.REACT_APP_MOVIES_API_URL, {
+  "content-type": "application/json",
+});
+
+export default moviesApi;
